66b241245ed102300d20b3fabddfad09e31a1ed4
Gülvera Yazılıtaş <98089865+RoseVera@users.noreply.github.com>
2025-07-10 11:41:44 +0300

    Update airflow_dag.py

--- dags/airflow_dag.py
+++ dags/airflow_dag.py
@@ -17,8 +17,7 @@ def fetch_data(**context):
 
     df = pd.DataFrame(data, columns=[
         'open_time', 'open', 'high', 'low', 'close', 'volume',
-        'close_time', 'quote_asset_volume', 'number_of_trades',
-        'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'
+        'close_time'
     ])
 
     df['open_time'] = pd.to_datetime(df['open_time'], unit='ms')
